1b0a8b57e1523c97f22e4e53b5bf5d00df18e72c
mnap <learn@10g.pl>
2018-03-31 20:29:04 +0200

    Add response with file and image

--- index.js
+++ index.js
@@ -1,16 +1,26 @@
+var fs = require('fs');
 var http = require('http');
 
 var server = http.createServer();
 
 server.on('request', function (request, response) {
     response.setHeader('Content-Type', 'text/html; charset=utf-8');
-    if (request.method === 'GET' && request.url === '/hello') {
+    if (request.method === 'GET' && request.url === '/') {
+        fs.readFile('./index.html', 'utf-8', function(err, data) {
+            if (err) throw err;
+            response.write(data);
+            response.end();
+        });
+    } else if (request.method === 'GET' && request.url === '/hello') {
         response.write('<h1>Hello world!</h1>');
         response.end();
     } else {
-        response.statusCode = 404;
-        response.write('<h1>404: Bad address</h1>');
-        response.end();
+        response.writeHead(404, {'Content-Type': 'image/png'});
+        fs.readFile('./images/404.png', function(err, data) {
+            if (err) throw err;
+            //response.write(data);
+            response.end(data);
+        });
     }
 });
 
