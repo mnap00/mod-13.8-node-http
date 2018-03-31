70f4cabf642b147c3b4c78877af49c221c44599d
mnap <learn@10g.pl>
2018-03-31 19:54:54 +0200

    Add 'hello' and '404' responses

--- index.js
+++ index.js
@@ -1,10 +1,17 @@
 var http = require('http');
 
 var server = http.createServer();
+
 server.on('request', function (request, response) {
-    response.write('<body>');
-    response.write('<h1>Hello world!</h1>');
-    response.write('</body>');
-    response.end();
+    response.setHeader('Content-Type', 'text/html; charset=utf-8');
+    if (request.method === 'GET' && request.url === '/hello') {
+        response.write('<h1>Hello world!</h1>');
+        response.end();
+    } else {
+        response.statusCode = 404;
+        response.write('<h1>404: Bad address</h1>');
+        response.end();
+    }
 });
+
 server.listen(9000);
